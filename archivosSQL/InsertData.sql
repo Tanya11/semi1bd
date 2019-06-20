c673761ca536c6dec26eab285ac0ec9b85f5e229
Tanya Muhun <Tanyarecinos911@gmail.com>
2019-06-19 22:48:25 -0600

    scripts_sql
    archivos con los scripts de sql para la creacion de la tabla y la insercion de datos en la bd.

---
+++ archivosSQL/InsertData.sql
@@ -0,0 +1,12 @@
+INSERT INTO product (sku,name,price,shortDescription,longDescription,active)
+VALUES ('000000','Computadora hp',250000.00,'shortDescription','longdescription','1');
+
+INSERT INTO product (sku,name,price,shortDescription,longDescription,active)
+VALUES ('000001','Samsung A10',2500.00,'shortDescription','longdescription','1');
+
+INSERT INTO product (sku,name,price,shortDescription,longDescription,active)
+VALUES ('000002','Huawei p9',1500.00,'shortDescription','longdescription','1');
+
+INSERT INTO product (sku,name,price,shortDescription,longDescription,active)
+VALUES ('000003','Shampoo',25.00,'shortDescription','longdescription','1');
+
